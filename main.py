495cb68908ae171d340ef91fc53e65bdf933c2db
Sean Thompson <seanthompson332@gmail.com>
2024-07-01 18:57:26 -0400

    working on line isomorphism checkes

--- main.py
+++ main.py
@@ -2,25 +2,48 @@ import itertools
 import numpy
 
 class GameState:
-    def __init__(self, num_dots, line_end_dot_pairs, face_border_line_dot_pairs) -> None:
+    def __init__(self, num_dots, line_end_dot_pairs, face_border_line_dot_pairs, faces_dots) -> None:
         self.num_dots = num_dots
-        self.line_end_dot_pairs = line_end_dot_pairs
-        self.face_border_line_dot_pairs = face_border_line_dot_pairs
+        self.lines = line_end_dot_pairs
+        self.faces = face_border_line_dot_pairs
+        self.faces_dots = faces_dots
 
-def paths_through_permutations():
-    pass
+def are_lines_iso(g1, g2):
+    unshaped_associations = numpy.array(list(itertools.product(list(range(1,g1.num_dots+1)), list(range(1,g1.num_dots+1)))))
+    unshaped_associations = list(unshaped_associations.reshape(g1.num_dots,g1.num_dots,2))
+
+    routes = list(itertools.permutations(list(range(0, len(unshaped_associations[0])))))
+
+    associations = []
+    for route in routes:
+        association = []
+        for j in route:
+            association.append(unshaped_associations[j][route[j]])
+        association= sorted(association, key=lambda x: x[0])
+        associations.append(association)
+
+    
+    for association in associations:
+        print(association)
+
+        for line in g1.lines:
+            start_dot_g1 = line[0]
+            end_dot_g1 = line[1]
+            print(start_dot_g1, end_dot_g1)
+            #LEFT OFF HERE
 
 def are_games_iso(g1, g2):
     if (    (g1.num_dots != g2.num_dots) or
-            (len(g1.line_end_dot_pairs) != len(g2.line_end_dot_pairs)) or
-            (len(g1.face_border_line_dot_pairs) != len(g2.face_border_line_dot_pairs))):
+            (len(g1.lines) != len(g2.lines)) or
+            (len(g1.faces) != len(g2.faces)) or
+            (len(g1.faces_dots) != len(g2.faces_dots))):
         return False
-    
-    all_associations = numpy.array(list(itertools.product(list(range(1,g1.num_dots+1)), list(range(1,g1.num_dots+1)))))
 
-    all_associations = all_associations.reshape(5,5,2)
-    print(all_associations)
+    if not are_lines_iso(g1, g2):
+        return False
 
-    routes = list(itertools.permutations(list(range(0, len(all_associations[0])))))
+g1 = GameState(4, [(1,2), (2,3), (2,4), (4,5)], [], [])
 
-are_games_iso(GameState(5, [], []), GameState(5, [], []))
+g2 = GameState(4, [(3,4), (4,5), (2,1), (4,2)], [], [])
+
+are_games_iso(g1, g2)
