23d4a463642a3eb65ad987bac91e6f2247195df7
Sean Thompson <seanthompson332@gmail.com>
2024-07-02 17:22:26 -0400

    did sum idk

--- genmoves.py
+++ genmoves.py
@@ -1,37 +1,45 @@
 import itertools
 
 def find_paths(d1, d2, lines, path):
+    _lines = lines.copy()
     if d1 == d2:
         return path
     else:
-        for line in lines:
+        for line in _lines:
             if line[0] == d1:
-                lines.remove(line)
+                _lines.remove(line)
                 path.append(line[1])
-                return find_paths(line[1], d2, lines, path)
+                return find_paths(line[1], d2, _lines, path)
             if line[1] == d1:
-                lines.remove(line)
+                _lines.remove(line)
                 path.append(line[0])
-                return find_paths(line[0], d2, lines, path)
+                return find_paths(line[0], d2, _lines, path)
 
 def find_group(d, dots, lines):
     dots_in_group = [d]
     for dot in dots:
-        path_exists = find_paths(d, dot, lines, [])
-        if path_exists:
-            dots_in_group.append(dot)
+        if d != dot:
+            path = find_paths(d, dot, lines, [])
+            if path != None:
+                dots_in_group.append(dot)
     return dots_in_group
 
 def find_isolated_groups(g):
     groups = []
     for i in range(0, g.num_dots):
         for j in range(0, g.num_dots):
-            path = find_paths(i, j, g.lines)
+            if i != j:
+                path = find_paths(i, j, g.lines, [])
 
-            # FIX THIS
-            if not is_path:
-                groups.append(find_group(i))
-                groups.append(find_group(j))
+                if path == None:
+                    print(i, j)
+                    print(g.lines)
+                    groups.append(find_group(i, range(0, g.num_dots), g.lines))
+                    break
+    for group_a in groups:
+        for group_b in groups:
+            if group_a != group_b and set(group_a) == set(group_b):
+                groups.remove(group_b)
     return groups
 
 def generate_moves(g):
@@ -43,6 +51,10 @@ def generate_moves(g):
                 if i in itertools.chain.from_iterable(g.faces_dotss) and j in itertools.chain.from_iterable(g.faces_dotts):
                     pass
 
-                # if isolated groups, use 2^n thing
-                else:
+                # if one is in and one isnt
+                elif i in itertoolsitertools.chain.from_iterable(g.faces_dotss) and (j in itertools.chain.from_iterable(g.faces_dotss)) == False:
+                    pass
+
+                # if both are on outside
+                elif (i in itertools.chain.from_iterable(g.faces_dotss)) == False and (j in itertools.chain.from_iterable(g.faces_dotts)) == False:
                     pass
